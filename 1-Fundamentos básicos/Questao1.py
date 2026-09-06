c14e8b47e2fcc0f766c1b7efac0d8e923f5992ed
ErikaMBarroso <erikambarroso@gmail.com>
2026-09-06 18:57:33 -0300

    Questao1

--- 1-Fundamentos básicos/Questao1.py
+++ 1-Fundamentos básicos/Questao1.py
@@ -1,1 +1,31 @@
 # Cadastro e apresentação de perfil pessoal
+
+nome = input("Digite seu nome completo:")
+
+while True:
+    idade = input("Digite sua idade: ")
+    try:
+        idade = int(idade)
+
+        if idade > 0:
+            break
+        else:
+            print("Valor digitado inválido.")
+    except ValueError:
+        print("Valor digitado inválido.")
+
+while True:
+    altura = input("Digite sua altura: ")
+    try:
+        altura = float(altura.replace(",", "."))
+
+        if altura > 0:
+            break
+        else:
+            print("Valor digitado inválido.")
+    except ValueError:
+        print("Valor digitado inválido.")
+
+cidade = input("Digite a cidade de residência:")
+
+print(f"|Nome:{nome}\n|Idade: {idade}\n|Altura: {altura}\n|Cidade:{cidade}")
