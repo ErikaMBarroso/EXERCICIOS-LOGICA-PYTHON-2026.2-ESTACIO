ea97db6c5575f821201c3426a8873290d0c8526f
ErikaMBarroso <erikambarroso@gmail.com>
2026-09-07 14:03:36 -0300

    Questao3

--- 1-Fundamentos básicos/Questao3.py
+++ 1-Fundamentos básicos/Questao3.py
@@ -1,1 +1,14 @@
 # Conversão entre escalas termométricas
+
+def celciusParaFahrenheit(tempC):
+    tempF = tempC * 9 / 5 + 32
+    print(f"{tempC:.2f} °C = {tempF:.2f} °F")
+
+def celciusParaKelvin(tempC):
+    tempK = tempC + 273.15
+    print(f"{tempC:.2f} °C = {tempK:.2f} K")
+
+tempC = float(input("Digite a temperatura em Celsius:").replace(',','.'))
+
+celciusParaFahrenheit(tempC)
+celciusParaKelvin(tempC)
