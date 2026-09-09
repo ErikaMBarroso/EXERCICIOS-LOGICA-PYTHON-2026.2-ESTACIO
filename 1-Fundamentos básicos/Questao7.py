03402638e5a4e94cd215dc6c64ce7076077dfffa
ErikaMBarroso <erikambarroso@gmail.com>
2026-09-09 15:42:56 -0300

    Questao7

--- 1-Fundamentos básicos/Questao7.py
+++ 1-Fundamentos básicos/Questao7.py
@@ -1,1 +1,16 @@
 # Análise simultânea de sinal e paridade
+
+numero = int(input("digite um número: "))
+
+if numero == 0:
+    print("O número é nulo")
+elif numero > 0:
+    if numero % 2 == 0:
+        print("O número é positivo e par")
+    else:
+        print("O número é positivo e ímpar")
+elif numero < 0:
+    if numero % 2 == 0:
+        print("O número é negativo e par")
+    else:
+        print("O número é negativo e ímpar")
