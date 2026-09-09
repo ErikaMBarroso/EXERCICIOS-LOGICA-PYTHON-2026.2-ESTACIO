18b04a428df4bb62f317eed67e68a6095dc79810
ErikaMBarroso <erikambarroso@gmail.com>
2026-09-09 15:06:34 -0300

    Questao5

--- 1-Fundamentos básicos/Questao5.py
+++ 1-Fundamentos básicos/Questao5.py
@@ -1,1 +1,14 @@
 # Classificação etária com validação de entrada
+
+idade = int(input("Digite sua idade: "))
+while idade < 0:
+    idade = int(input("Digite uma idade válida: "))
+
+if idade <= 12:
+    print("Você é classificado como criança")
+elif idade < 18:
+    print("Você é classificado como adolesccente")
+elif idade < 60:
+    print("Você é classificado como adulto")
+else: 
+    print("Você é classificado como idoso")
