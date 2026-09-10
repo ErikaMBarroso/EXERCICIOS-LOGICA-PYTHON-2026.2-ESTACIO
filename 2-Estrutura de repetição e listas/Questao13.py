c8c2a51f343f3d32fb07e6d61ab24622eabf4c42
ErikaMBarroso <erikambarroso@gmail.com>
2026-09-10 18:12:38 -0300

    Questao13

--- 2-Estrutura de repetição e listas/Questao13.py
+++ 2-Estrutura de repetição e listas/Questao13.py
@@ -1,1 +1,31 @@
 # Agenda de contatos com funcionalidade de consulta
+
+contatos = []
+
+for c in range(1, 6):
+    nome = input(f"Digite o nome do {c}° contato: ")
+    telefone = input(f"Digite o número do contato {nome}: ")
+    email = input(f"Digite o email do contato {nome}: ")
+
+    novoContato = {
+        "nome": nome,
+        "telefone": telefone,
+        "email": email
+    }
+
+    contatos.append(novoContato)
+
+
+contatoBuscado = input("Qual contato você deseja consultar?\n")
+
+encontrado = False
+
+for contato in contatos:
+    if contato["nome"].lower() == contatoBuscado.lower():
+        print(f"Nome: {contato['nome']}\nTelefone: {contato['telefone']}\nEmail: {contato['email']}")
+        encontrado = True
+        break
+
+
+if not encontrado:
+    print("Contato não encontrado.")
